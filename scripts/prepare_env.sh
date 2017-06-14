f2128fa6aa0b0b433f7728e33eba7408706e9bc5
Serhii Sokolov <sokolov.job@gmail.com>
2017-06-14 12:48:57 +0300

    use cmake for all subprojects

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo apt-get install python-xattr cmake automake autoconf libtool bison gcc g++ libboost-all-dev libboost-dev python libicu-dev openssl libssl-dev checkinstall \
 apt-transport-https ca-certificates curl software-properties-common linux-image-extra-$(uname -r) linux-image-extra-virtual libprotobuf-dev protobuf-compiler pkg-config \
-google-mock libgflags-dev libgtest-dev clang libc++-dev
+google-mock libgflags-dev libgtest-dev clang libc++-dev golang-any
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
-sudo apt-get install docker-ce
+#sudo apt-get update
+#sudo apt-get install docker-ce
 
 pushd ..
 
@@ -22,12 +22,12 @@ pushd third_party/protobuf
 git checkout v3.2.0 || exit -1
 ./autogen.sh || exit -1
 ./configure || exit -1
-make -j$(nproc --all) || exit -1
-make install || exit -1
+#make -j$(nproc --all) || exit -1
+#make install || exit -1
 
 popd
-make -j$(nproc --all) || exit -1
-make install || exit -1
+#make -j$(nproc --all) || exit -1
+#make install || exit -1
 
 popd
 
