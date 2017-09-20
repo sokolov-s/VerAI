a136483f7556de9ca25e9611ab8b96db19444e02
Serhii Sokolov <sokolov.job@gmail.com>
2017-09-20 16:34:40 +0300

    finish creating model and runing it

--- tensorflow/components/base.py
+++ tensorflow/components/base.py
@@ -14,16 +14,26 @@ class Base(metaclass=ABCMeta):
         VALUE = "value"
         DESC = "description"
         IMPORTANT = "important"
-
+    
+    class ComponentProperties:
+        VERSION = "version"
+        TYPE = "component_type"
+        TIMESTAMP = "timestamp"
+        
     def __init__(self, tf_session, name):
-        self.name = name
-        self.inputs = dict()
-        self.outputs = dict()
-        self.params = dict()
-        self.sess = tf_session
+        self.__name = name
+        self.__inputs = dict()
+        self.__outputs = dict()
+        self.__params = dict()
+        self.__sess = tf_session
+        self.__properties = {
+            self.ComponentProperties.VERSION: None,
+            self.ComponentProperties.TYPE: None,
+            self.ComponentProperties.TIMESTAMP: None
+            }
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_class_name(self):
         return self.__class__.__name__
@@ -50,53 +60,74 @@ class Base(metaclass=ABCMeta):
         return obj[name][Base.ParamType.VALUE]
 
     def add_input(self, name: str, value=None, desc="", important=True):
-        self._add_param_to_obj(obj=self.inputs, name=name, value=value, desc=desc, important=important)
+        self._add_param_to_obj(obj=self.__inputs, name=name, value=value, desc=desc, important=important)
 
     def set_input(self, name, value):
-        self._set_param_value_in_obj(obj=self.inputs, name=name, value=value)
+        self._set_param_value_in_obj(obj=self.__inputs, name=name, value=value)
 
     def set_inputs(self, inputs: dict):
-        self._set_params_value_in_obj(obj=self.inputs, params=inputs)
+        self._set_params_value_in_obj(obj=self.__inputs, params=inputs)
 
     def get_input(self, name):
-        return self._get_param_value_from_obj(self.inputs, name)
+        return self._get_param_value_from_obj(self.__inputs, name)
 
     def get_inputs(self):
-        return self.inputs
+        return self.__inputs
 
     def add_output(self, name: str, value=None, desc=""):
-        self._add_param_to_obj(obj=self.outputs, name=name, value=value, desc=desc)
+        self._add_param_to_obj(obj=self.__outputs, name=name, value=value, desc=desc)
 
     def set_output(self, name, value):
-        self._set_param_value_in_obj(obj=self.outputs, name=name, value=value)
+        self._set_param_value_in_obj(obj=self.__outputs, name=name, value=value)
 
     def set_outputs(self, outputs: dict):
-        self._set_params_value_in_obj(obj=self.outputs, params=outputs)
+        self._set_params_value_in_obj(obj=self.__outputs, params=outputs)
 
     def get_output(self, name):
-        return self._get_param_value_from_obj(self.outputs, name)
+        return self._get_param_value_from_obj(self.__outputs, name)
 
     def get_outputs(self):
-        return self.outputs
+        return self.__outputs
 
     def add_param(self, name: str, value=None, desc="", important=True):
-        self._add_param_to_obj(obj=self.params, name=name, value=value, desc=desc, important=important)
+        self._add_param_to_obj(obj=self.__params, name=name, value=value, desc=desc, important=important)
 
     def set_param(self, name, value):
-        self._set_param_value_in_obj(obj=self.params, name=name, value=value)
+        self._set_param_value_in_obj(obj=self.__params, name=name, value=value)
 
     def set_params(self, params: dict):
-        self._set_params_value_in_obj(obj=self.params, params=params)
+        self._set_params_value_in_obj(obj=self.__params, params=params)
 
     def get_param(self, name):
-        return self._get_param_value_from_obj(self.params, name)
+        return self._get_param_value_from_obj(self.__params, name)
 
     def get_params(self):
-        return self.params
+        return self.__params
 
     def get_session(self):
-        return self.sess
+        return self.__sess
     
+    def __check_property(self, property: ComponentProperties):
+        if property in self.get_properties():
+            return True
+        else:
+            raise ValueError("%s is not a module property" % property)
+    
+    def get_property(self, property: ComponentProperties):
+        self.__check_property(property)
+        return self.__properties[property]
+    
+    def get_properties(self):
+        return self.__properties
+    
+    def set_property(self, property: ComponentProperties, value):
+        self.__check_property(property)
+        self.__properties[property] = value
+    
+    def set_properties(self, properties: dict):
+        for key, value in properties.items():
+            self.set_property(key, value)
+        
     @staticmethod
     def to_tf_dtype(dtype: str, default=None):
         res = tf.as_dtype(dtype) if dtype else default
--- tensorflow/main.py
+++ tensorflow/main.py
@@ -9,8 +9,14 @@ from collections import OrderedDict
 from component_loader import ComponentLoader
 import tensorflow as tf
 from asyncore import read
+from plainbox.impl import result
 
 json_file = None
+created_components_outputs = dict()
+components_instance = dict()
+components_test_json = dict()
+components_train_json = dict()
+dataset = None
 
 def print_no_argument_and_exit():
     print("Bad parameters")
@@ -44,13 +50,13 @@ Options:
             print("Unknown parameter %s" % opt)
             exit(2)
     
-def set_inputs(module_instance, json_item, created_models):
+def set_inputs(module_instance, json_item, created_components):
     if "inputs" in json_item and json_item["inputs"]:
         for dependence in json_item["inputs"]:
             dep_obj_name, dep_var_name = str(dependence["value"]).split('.')
-            if dep_obj_name in created_models.keys():
-                if dep_var_name in created_models[dep_obj_name].keys():
-                    module_instance.set_input(dependence["name"], created_models[dep_obj_name][dep_var_name])
+            if dep_obj_name in created_components.keys():
+                if dep_var_name in created_components[dep_obj_name].keys():
+                    module_instance.set_input(dependence["name"], created_components[dep_obj_name][dep_var_name])
                 else:
                     raise ValueError("Ca't find dependence value")
                     break
@@ -64,7 +70,18 @@ def set_params(module_instance, json_item):
             except Exception as ex:
                 print("\033[91m%s\033[0m" % ex)
 
-
+def set_propertie(module_instance, json_item, property_name):
+    if property_name in json_item:
+        try:
+            module_instance.set_property(property_name, json_item[property_name])
+        except Exception as ex:
+            print("\033[91m%s\033[0m" % ex)
+        
+def set_properties(module_instance, json_item):
+    set_propertie(module_instance, json_item, module_instance.ComponentProperties.VERSION)
+    set_propertie(module_instance, json_item, module_instance.ComponentProperties.TYPE)
+    set_propertie(module_instance, json_item, module_instance.ComponentProperties.TIMESTAMP)
+    
 def load_json(file):    
     with open(file) as fp:
         try:
@@ -106,42 +123,122 @@ def check_dependences(json_item, created_components):
                               (obj_name, var_name))
                         return False
     return True
-                        
-def main():
-    parse_argv()
-    global json_file
-    print(json_file)
-    if json_file is None or not os.path.isfile(json_file):
-        print("Can't find json file")
-        exit(2)
-        
-    js_data = load_json(json_file)
+
+def create_components(js_data):
     json_items = list(js_data.keys())
-    
     modules = ComponentLoader()
-    created_models = {}
-    sess = tf.Session()
+    global created_components_outputs
+    global components_train_json
+    global components_test_json
+    global dataset
     i = 0
     while len(json_items) > 0:
         item_name = json_items[i]
-        item = js_data[item_name]
+        json_item = js_data[item_name]
         
-        print("Parse object %s :%s" % (item_name, item))
-        ready_to_create = check_dependences(item, created_models)
+        print("Parse object %s :%s" % (item_name, json_item))
+        ready_to_create = check_dependences(json_item, created_components_outputs)
         if ready_to_create:
-            print("Create object %s :%s" % (item_name, item))
-            new_obj = modules.get_component_instance(class_name=item["class"], version=item["version"], name=item_name,
-                                                 tf_session=sess)
+            print("Create object %s :%s" % (item_name, json_item))
+            new_obj = modules.get_component_instance(class_name=json_item["class"], version=json_item["version"], name=item_name,
+                                                 tf_session=None)
             if not new_obj:
                 raise ValueError("Can't find module instance: class_name = %s, version = %s" %
-                                 (item["class"], item["version"]))
-            set_inputs(new_obj, item, created_models)
-            set_params(new_obj, item)
+                                 (json_item["class"], json_item["version"]))
+            set_inputs(new_obj, json_item, created_components_outputs)
+            set_params(new_obj, json_item)
+            set_properties(new_obj, json_item)
             results = new_obj.init()
-            created_models[item_name] = results
+            
+            created_components_outputs[item_name] = results
+            components_instance[item_name] = new_obj
+            if new_obj.get_property(new_obj.ComponentProperties.TYPE) == "train":
+                components_train_json[item_name] = json_item
+            elif new_obj.get_property(new_obj.ComponentProperties.TYPE) == "test":
+                components_test_json[item_name] = json_item
+            
+            if json_item["class"] == "DataSet":
+                dataset = new_obj
+             
             json_items.remove(item_name)
     
         i = i + 1 if i < len(json_items) - 1 else 0
+
+def get_dependence_names(json_item):
+    res = list()
+    if "inputs" in json_item and json_item["inputs"]:
+        for dependence in json_item["inputs"]:
+            dep_obj_name, dep_var_name = str(dependence["value"]).split('.')
+            if dep_obj_name:
+                res.append(dep_obj_name)
+    return res
+    
+def get_components_to_run(components):
+    global created_components_outputs
+    component_names = set(components.keys())
+    dependence_names = set()
+    for component_name, json_value in components.items():
+        names = set(get_dependence_names(json_value))
+        dependence_names.update(names)
+    components_to_run = component_names.difference(set(dependence_names))
+    res = list()
+    for component_name in components_to_run:
+        outputs = created_components_outputs[component_name]
+        for tf_component in outputs.values(): 
+            res.append(tf_component) 
+    return res
+
+def get_dataset(components):
+    #TODO: looking for all input parameters and find datasets 
+    global dataset
+    res = list()
+    res.append(dataset)
+    return res
+    
+def run_train(session):
+    global components_train_json;
+    components = get_components_to_run(components_train_json)
+    datasets = get_dataset(components)
+    feed_dicts = {}
+    for dataset in datasets:
+        dataset.get_handler().switch_iteration(dataset.get_handler().Iterations.TRAIN)
+    for _ in range(1000):
+        for dataset in datasets:
+            feed_dict = dataset.get_handler().fill_next_feed_dict(100)
+            feed_dicts.update(feed_dict)
+        session.run(components, feed_dict=feed_dicts)
+
+def run_test(session):
+    global components_test_json;
+    components = get_components_to_run(components_test_json)
+    datasets = get_dataset(components)
+    feed_dicts = {}
+    for dataset in datasets:
+        dataset.get_handler().switch_iteration(dataset.get_handler().Iterations.TEST)
+    for dataset in datasets:
+        feed_dict = dataset.get_handler().fill_next_feed_dict(100)
+        feed_dicts.update(feed_dict)
+    print(session.run(components, feed_dict=feed_dicts))
+
+def run_model():
+    sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
+    run_train(sess)
+    run_test(sess)
+         
+def main():
+    parse_argv()
+    global json_file
+    print("Load model from %s" % json_file)
+    if json_file is None or not os.path.isfile(json_file):
+        print("Can't find json file")
+        exit(2)
+    js_data = load_json(json_file)
+    
+    create_components(js_data)
+    
+    run_model()
+    
 
 main()
 print("Model has been built")
